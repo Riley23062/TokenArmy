7ce4893aa368c8a604dc8397f3f1a99152a3842f
Riley23062 <82524320+Riley23062@users.noreply.github.com>
2024-06-15 17:06:55 -0400

    Basic list view

--- app.js
+++ app.js
@@ -4,27 +4,44 @@ const ejs = require('ejs');
 const port = 9000;
 var app = express();
 
-
-let list = []
-scryfall.Cards.search('type:token').get(687).then((value) => {
+let tokenList = []
 
 
-   for (let i = 0; i < value.length; i++) {
-   
-       for (const key in value[i]) {
-        if(key == "name"){
-
-            list.push(value[i].name)
-        }
+class Token {
+    constructor(name, colors, power, toughness, image){
+        this.name = name
+        this.colors = colors
+        this.power = power
+        this.toughness = toughness
+        this.amount = 0
+        this.image = image
     }
+}
+
+scryfall.Cards.search('type:token').get(687).then((value) => {
+    let imageURI 
+    
+    //console.log(value);
+   for (let i = 0; i < value.length; i++) {
+
+
+if(Object.hasOwn(value[i], 'image_uris')){
+
+imageURI = value[i].image_uris.small
+
+tokenList.push(new Token(value[i].name, value[i].color_identity, value[i].power, value[i].toughness, imageURI))
+} else {
+
+    tokenList.push(new Token(value[i].name, value[i].color_identity, value[i].power, value[i].toughness, ''))
+}
+
+   
+
          
          }
-         
-        console.log(list);
-          
+        
+
   });
-
-
 
 
   
@@ -32,17 +49,19 @@ scryfall.Cards.search('type:token').get(687).then((value) => {
 
 
 
-//app.use(express.urlencoded({ extended: true }));
-//app.set('view engine', 'ejs');
-//
-//
-//app.get('/', (req, res) => {
-//
-//    res.send('Hello World!')
-//
-//})
-//
-//app.listen(port, () => {
-//console.log('Server running on port ' + port)
-//
-//});
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        list: tokenList
+    })
+
+})
+
+app.listen(port, () => {
+console.log('Server running on port ' + port)
+
+});
